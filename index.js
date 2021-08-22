698b26e450fc6380836ca517670e56bf52479eb6
Lenfumé Julien <julabina@hotmail.fr>
2021-08-22 11:51:31 +0200

    22 08 21 11 51

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const weightInput = document.getElementById("weight");
-const sizeInput = document.getElementById("size");
+const inputs = document.querySelectorAll("#weight, #size");
 const resultBoxe = document.querySelector(".BMIResult");
+const resultBtn = document.getElementById("btn");
 let weight, size, result;
 
 const errorDisplay = (tag, message, valid) => {
@@ -17,13 +17,61 @@ const errorDisplay = (tag, message, valid) => {
 };
 
 const bmiCalc = () => {
+  let firstResult;
   sizeSquared = size ** 2;
-  result = weight / sizeSquared;
+  firstResult = weight / sizeSquared;
+  result = Math.round(firstResult * 10) / 10;
   return result;
 };
 
 const displayBmi = () => {
+  const para = document.getElementById("paraResult");
   resultBoxe.innerHTML = `
     <h2>${result}</h2>
     `;
+  para.classList.remove("paraVisible");
 };
+
+const weightInputCheck = (value) => {
+  if (value < 10) {
+    console.log("test1");
+  } else if (!value.match(/^[1-9]\d*$/i)) {
+    console.log("test2");
+  } else {
+    console.log("test3");
+    weight = value;
+  }
+};
+
+const sizeInputCheck = (value) => {
+  if (value < 10) {
+    console.log("test1");
+  } else if (!value.match(/^[1-9]\d*$/i)) {
+    console.log("test2");
+  } else {
+    let valueDiv;
+    valueDiv = value / 100;
+    size = valueDiv;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "weight":
+        weightInputCheck(e.target.value);
+        break;
+      case "size":
+        sizeInputCheck(e.target.value);
+        break;
+      default:
+        null;
+    }
+  });
+});
+
+resultBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  bmiCalc();
+  displayBmi();
+});
